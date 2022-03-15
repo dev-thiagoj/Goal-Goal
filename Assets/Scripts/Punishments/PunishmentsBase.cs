249ab7468bc5b12a53c367fb6da478a43f5f64b5
Dev.ThiagoJ <dev.thiagoj@gmail.com>
2022-03-15 16:27:31 -0300

    Start the punishment list in portuguese

--- Assets/Scripts/Punishments/PunishmentsBase.cs
+++ Assets/Scripts/Punishments/PunishmentsBase.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PunishmentsBase : MonoBehaviour
 {
+    public string[] punishments;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        punishments[0] = "Colocar dois dedos dentro de uma narina";
+        punishments[1] = "Girar 10x e no fim por o indicador na ponta do nariz";
+        punishments[2] = "Fazer 10 polichinelos";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach(string punishment in punishments){
+
+            Debug.Log(punishment);
+        }
     }
 }
