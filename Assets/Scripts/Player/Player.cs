2b4ec1f244b8164c6dbb4b9b0165c249d667cd2b
Dev.ThiagoJ <dev.thiagoj@gmail.com>
2022-06-07 16:03:59 -0300

    [fix] fix players bounds

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,25 +1,33 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
 
 public class Player : MonoBehaviour
 {
-    public Player player; 
+    public Player player;
     //public CharacterController characterController;
     public Rigidbody2D rigidbody2D;
-   
+
     public Image uiPlayer;
     public string playerName;
 
-    [Header("Limits")]
-    public Vector2 limitsX = new Vector2(-4f, 4f);
-    public Vector2 limitsY = new Vector2(-4f, 4f);
+    [Header("Bounds")]
+    //public Vector2 limitsX = new Vector2(-4f, 4f);
+    //public Vector2 limitsY = new Vector2(-4f, 4f);
+    public Transform rightBoundTransform;
+    private Vector3 rightBoundVec;
+    public Transform leftBoundTransform;
+    private Vector3 leftBoundVec;
+    public Transform upBoundTransform;
+    private Vector3 upBoundVec;
+    public Transform downBoundTransform;
+    private Vector3 downBoundVec;
 
     [Header("Texts")]
     public TextMeshProUGUI uiTextPoints;
     //public TextMeshProUGUI uiTextEndGame;
 
-    
     public int currentPoints;
 
     private Vector3 _pos;
@@ -32,16 +40,19 @@ public class Player : MonoBehaviour
 
     private void Awake()
     {
-        
         ResetPlayer();
         //initialPosition = transform.position;
+    }
+
+    private void Start()
+    {
+        GetBounds();
     }
 
     void Update()
     {
         Bounds();
         FinalPoint();
-        
     }
 
     public void ResetPlayer()
@@ -50,20 +61,34 @@ public class Player : MonoBehaviour
         UpdateUI();
     }
 
+    private void GetBounds()
+    {
+        rightBoundVec = rightBoundTransform.transform.position;
+        leftBoundVec = leftBoundTransform.transform.position;
+        upBoundVec = upBoundTransform.transform.position;
+        downBoundVec = downBoundTransform.transform.position;
+    }
+
     public void Bounds()
     {
         _pos.y = rigidbody2D.transform.position.y;
         _pos.x = rigidbody2D.transform.position.x;
 
-        if (_pos.x < limitsX.x) _pos.x = limitsX.x;
-        else if (_pos.x > limitsX.y) _pos.x = limitsX.y;
+        ///if (_pos.x < limitsX.x) _pos.x = limitsX.x;
+        //else if (_pos.x > limitsX.y) _pos.x = limitsX.y;
 
-        if (_pos.y < limitsY.x) _pos.y = limitsY.x;
-        else if (_pos.y > limitsY.y) _pos.y = limitsY.y;
+        //if (_pos.y < limitsY.x) _pos.y = limitsY.x;
+        //else if (_pos.y > limitsY.y) _pos.y = limitsY.y;
+
+        if (_pos.x > rightBoundVec.x) _pos.x = rightBoundVec.x;
+        else if (_pos.x < leftBoundVec.x) _pos.x = leftBoundVec.x;
+
+        if (_pos.y > upBoundVec.y) _pos.y = upBoundVec.y;
+        else if (_pos.y < downBoundVec.y) _pos.y = downBoundVec.y;
 
         rigidbody2D.transform.position = _pos;
     }
-    
+
     /*public void SetName(string s)
     {
         player.name = s;
@@ -77,7 +102,7 @@ public class Player : MonoBehaviour
     public void AddPoint()
     {
         currentPoints++;
-        UpdateUI();        
+        UpdateUI();
     }
 
     private void UpdateUI()
@@ -91,6 +116,14 @@ public class Player : MonoBehaviour
         {
             //player.name = (string) playerName;
             GameManager.Instance.ChangeStateToEnd();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Wall"))
+        {
+            
         }
     }
 }
